af9812c89723c8dbf3800823637a5cebd6d738e6
nathaniel-k-green <nagreen778@gmail.com>
2024-08-29 20:04:57 -0500

    json

--- product.js
+++ product.js
@@ -1,7 +1,21 @@
-import data from './products.json' assert { type: 'json' };
+// import data from './products.json' assert { type: 'json' };
 
 function load_products() {
+    data = "hello"
     console.log(data)
+    fetch("products.json")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data); // Use the JSON data here
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
 }
 
 load_products()
