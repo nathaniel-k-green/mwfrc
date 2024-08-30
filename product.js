d38f01386c59e59f481507faa8c7318dc1c25965
nathaniel-k-green <nagreen778@gmail.com>
2024-08-29 20:46:57 -0500

    lenfefn

--- product.js
+++ product.js
@@ -5,7 +5,7 @@ function load_products() {
     console.log(data)
     // fetch("https://github.com/nathaniel-k-green/mwfrc/products.json")
     fetch("https://nathaniel-k-green.github.io/mwfrc/products.json")
-    // fetch("https://nathaniel-k-green.github.io/mwfrc/products.json")
+        // fetch("https://nathaniel-k-green.github.io/mwfrc/products.json")
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -14,6 +14,7 @@ function load_products() {
         })
         .then(data => {
             console.log(data); // Use the JSON data here
+            add_to_html(data);
         })
         .catch(error => {
             console.error('There has been a problem with your fetch operation:', error);
@@ -25,24 +26,24 @@ function load_products() {
 
 // }
 
-// function add_to_html(data) {
-//     for (product in data) {
-//         html = `<div class="col">
-//                     <div class="card h-100" style="width: 18rem;">
-//                         <img class="card-img-top" src="images/${"image"}" alt="Card image cap">
-//                         <div class="card-body">
-//                             <p id="is-new" style="color:coral; font-size: .75em;">NEW</p>
-//                             <h5 class="card-title">${product["name"]}</h5>
-//                             <h6 class="card-title">$${product["price"]}</h6>
-//                             <p class="card-text">${product["desc"]}</p>
-//                             <a href="https://www.etsy.com/" class="btn btn-primary">View</a>
-//                         </div>
-//                     </div>
-//                 </div>`
-//         first = document.getElementById("FIRST")
-//         first.appendChild(html)
-//     }
-    
-// }
+function add_to_html(data) {
+    for (product in data) {
+        html = `<div class="col">
+                    <div class="card h-100" style="width: 18rem;">
+                        <img class="card-img-top" src="images/${"image"}" alt="Card image cap">
+                        <div class="card-body">
+                            <p id="is-new" style="color:coral; font-size: .75em;">NEW</p>
+                            <h5 class="card-title">${product["name"]}</h5>
+                            <h6 class="card-title">$${product["price"]}</h6>
+                            <p class="card-text">${product["desc"]}</p>
+                            <a href="https://www.etsy.com/" class="btn btn-primary">View</a>
+                        </div>
+                    </div>
+                </div>`
+        first = document.getElementById("FIRST")
+        first.appendChild(html)
+    }
 
-load_products()
+    // }
+
+    load_products()
