ccc5be0c679a2706ea91ee9e2e664b7cc1947260
nathaniel-k-green <nagreen778@gmail.com>
2024-08-30 12:24:51 -0500

    added loading pics

---
+++ index.js
@@ -0,0 +1,105 @@
+
+
+// <div class="col">
+// <div class="card h-100" style="width: 18rem;">
+//     <!-- <img class="card-img-top" src="../images.png" alt="Card image cap"> -->
+//     <div class="card-body">
+//         <p id="is-new" style="color:coral; font-size: .75em;">NEW</p>
+//         <h5 class="card-title">Card title</h5>
+//         <h6 class="card-title">$5</h6>
+//         <p class="card-text">Some quick example text to build on the card title and make up
+//             the bulk of the card's content.</p>
+//         <a href="#" class="btn btn-primary">Go somewhere</a>
+//     </div>
+// </div>
+// </div>
+
+// <div class="col">
+// <div class="card h-100" style="width: 18rem;">
+//     <!-- <img class="card-img-top" src="../images.png" alt="Card image cap"> -->
+//     <div class="card-body">
+//         <h5 class="card-title">Card title</h5>
+//         <p class="card-text">Some quick example text to build on the card title and make up
+//             the bulk of the card's content.</p>
+//         <a href="#" class="btn btn-primary" style="margin-top: auto;">Go somewhere</a>
+//     </div>
+// </div>
+// </div>
+// <!-- </div> -->
+// <!-- <br> -->
+
+// <!-- <div class="row"> -->
+// <div class="col">
+// <div class="card h-100" style="width: 18rem;">
+//     <!-- <img class="card-img-top" src="../images.png" alt="Card image cap"> -->
+//     <div class="card-body">
+//         <h5 class="card-title">Card title</h5>
+//         <p class="card-text">Some quick example text to build on the card title and make up
+//             the bulk of the card's content.</p>
+//         <a href="#" class="btn btn-primary">Go somewhere</a>
+//     </div>
+// </div>
+// </div>
+
+// <div class="col">
+// <div class="card h-100" style="width: 18rem;">
+//     <!-- <img class="card-img-top" src="../images.png" alt="Card image cap"> -->
+//     <div class="card-body">
+//         <h5 class="card-title">Card title</h5>
+//         <p class="card-text">Some quick example text to build on the card title and make up
+//             the bulk of the card's content.</p>
+//         <a href="#" class="btn btn-primary">Go somewhere</a>
+//     </div>
+// </div>
+// </div>
+
+// <div class="col">
+// <div class="card h-100" style="width: 18rem;">
+//     <!-- <img class="card-img-top" src="../images.png" alt="Card image cap"> -->
+//     <div class="card-body">
+//         <h5 class="card-title">Card title</h5>
+//         <p class="card-text">Some quick example text to build on the card title and make up
+//             the bulk of the card's content.</p>
+//         <a href="#" class="btn btn-primary">Go somewhere</a>
+//     </div>
+// </div>
+// </div>
+// <!-- </div>
+// <br> -->
+
+// <!-- <div class="row"> -->
+// <div class="col">
+// <div class="card h-100" style="width: 18rem;">
+//     <!-- <img class="card-img-top" src="../images.png" alt="Card image cap"> -->
+//     <div class="card-body">
+//         <h5 class="card-title">Card title</h5>
+//         <p class="card-text">Some quick example text to build on the card title and make up
+//             the bulk of the card's content.</p>
+//         <a href="#" class="btn btn-primary">Go somewhere</a>
+//     </div>
+// </div>
+// </div>
+
+// <div class="col">
+// <div class="card h-100" style="width: 18rem;">
+//     <!-- <img class="card-img-top" src="../images.png" alt="Card image cap"> -->
+//     <div class="card-body">
+//         <h5 class="card-title">Card title</h5>
+//         <p class="card-text">Some quick example text to build on the card title and make up
+//             the bulk of the card's content.</p>
+//         <a href="#" class="btn btn-primary">Go somewhere</a>
+//     </div>
+// </div>
+// </div>
+
+// <div class="col">
+// <div class="card h-100" style="width: 18rem;">
+//     <!-- <img class="card-img-top" src="../images.png" alt="Card image cap"> -->
+//     <div class="card-body">
+//         <h5 class="card-title">Card title</h5>
+//         <p class="card-text">Some quick example text to build on the card title and make up
+//             the bulk of the card's content.</p>
+//         <a href="#" class="btn btn-primary">Go somewhere</a>
+//     </div>
+// </div>
+// </div>
--- product.js
+++ product.js
@@ -30,27 +30,28 @@ function add_to_html(data) {
     for (product of data) {
         // html = `<div class="col">
         
-        html = `
-                    <div class="card h-100" style="width: 18rem;">
-                        <img class="card-img-top" src="images/${product["image"]}" alt="Card image cap">
-                        <div class="card-img-overlay">
-                             <span class="badge rounded-pill text-bg-light" style="color: coral;">
-                                NEW
-                            </span>
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">${product["name"]}</h5>
-                            <h6 class="card-title">$${product["price"]}</h6>
-                            <p class="card-text">${product["desc"]}</p>
-                            <a href="https://www.etsy.com/" class="btn btn-primary">View</a>
-                        </div>
-                    </div>`
+        html = `<div class="card h-100" style="width: 18rem;">
+                    <img class="card-img-top" src="images/${product["image"]}" alt="Card image cap">
+                    <div class="card-img-overlay">
+                        <span class="badge rounded-pill text-bg-light">
+                            NEW
+                        </span>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">${product["name"]}</h5>
+                        <h6 class="card-title">$${product["price"]}</h6>
+                        <p class="card-text">${product["desc"]}</p>
+                        <a href="https://www.etsy.com/" class="btn btn-primary">View</a>
+                    </div>
+                </div>`
                 {/* </div>` */}
-        const first = document.getElementById("FIRST");
-        var x = document.createElement("div");
-        x.classList.add("col");
-        x.innerHTML = html;
-        first.appendChild(x);
+        const product_grid = document.getElementById("product_grid");
+        product_grid.innerHTML = '<div class="col">' + html + '</div>';
+        // const first = document.getElementById("FIRST");
+        // const x = document.createElement("div");
+        // x.classList.add("col");
+        // x.innerHTML = html;
+        // first.appendChild(x);
         console.log(html);
     }
 
