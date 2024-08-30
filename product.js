e093b111571f05ed0fec341348b79647b41d72b3
nathaniel-k-green <nagreen778@gmail.com>
2024-08-30 18:04:54 -0500

    added

--- product.js
+++ product.js
@@ -27,26 +27,27 @@ function load_products() {
 // }
 
 function add_to_html(data) {
+    html = ``
     for (product of data) {
         // html = `<div class="col">
         
-        html = `<div class="card h-100" style="width: 18rem;">
-                    <img class="card-img-top" src="images/${product["image"]}" alt="Card image cap">
-                    <div class="card-img-overlay">
-                        <span class="badge rounded-pill text-bg-light">
-                            NEW
-                        </span>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">${product["name"]}</h5>
-                        <h6 class="card-title">$${product["price"]}</h6>
-                        <p class="card-text">${product["desc"]}</p>
-                        <a href="https://www.etsy.com/" class="btn btn-primary">View</a>
+        html = html + `<div class="col">
+                    <div class="card h-100" style="width: 18rem;">
+                        <img class="card-img-top" src="images/${product["image"]}" alt="Card image cap">
+                        <div class="card-img-overlay">
+                            <span class="badge rounded-pill text-bg-light">
+                                NEW
+                            </span>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">${product["name"]}</h5>
+                            <h6 class="card-title">$${product["price"]}</h6>
+                            <p class="card-text">${product["desc"]}</p>
+                            <a href="https://www.etsy.com/" class="btn btn-primary">View</a>
+                        </div>
                     </div>
                 </div>`
-                {/* </div>` */}
-        const product_grid = document.getElementById("product_grid");
-        product_grid.innerHTML = '<div class="col">' + html + '</div>';
+        
         // const first = document.getElementById("FIRST");
         // const x = document.createElement("div");
         // x.classList.add("col");
@@ -54,6 +55,8 @@ function add_to_html(data) {
         // first.appendChild(x);
         console.log(html);
     }
+    const product_grid = document.getElementById("product_grid");
+    product_grid.innerHTML = html;
 
 }
 
